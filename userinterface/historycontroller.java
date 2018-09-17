bbd0e0eb1b2b50a277f6891fdac36b32fe1e5f94
TeamBool <VTSysteme2018>
2018-09-17 22:22:49 +0200

    Gui Update

---
+++ userinterface/historycontroller.java
@@ -0,0 +1,33 @@
+package userinterface;
+
+import javafx.application.Platform;
+import javafx.event.ActionEvent;
+import javafx.fxml.FXMLLoader;
+import javafx.scene.Node;
+import javafx.scene.Parent;
+import javafx.scene.Scene;
+import javafx.stage.Stage;
+
+public class historycontroller {
+
+
+
+    public void backbutton2(ActionEvent event) throws Exception {
+
+        {
+            Parent root = FXMLLoader.load(getClass().getClassLoader().getResource("userinterface/main.fxml"));
+            Stage stage = new Stage();
+            stage.setTitle(" ");
+            stage.setScene(new Scene(root, 600, 570));
+            stage.show();
+
+            ((Node) (event.getSource())).getScene().getWindow().hide();
+        }
+
+    }
+
+    public void doExit() {
+        Platform.exit();
+    }
+}
+
